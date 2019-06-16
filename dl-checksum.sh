3f740d29e612e8e8258cd4438d2ebbe8411fd3f4
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-06-15 23:53:57 -0500

    up to v2.12.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=2.11.2
+VER=2.12.0
 DIR=~/Downloads
 MIRROR=https://github.com/github/hub/releases/download
 
@@ -9,7 +9,10 @@ dl()
     PLATFORM=$2
     SUFFIX=$3
     FILE=hub-${OS}-${PLATFORM}-${VER}.${SUFFIX}
-    wget -O $DIR/$FILE $MIRROR/v${VER}/$FILE
+    DLFILE=$DIR/$FILE
+    URL=$MIRROR/v${VER}/$FILE
+    wget -q -O $DLFILE $URL
+    printf "# %s\n%s-%s: sha256:%s\n" $URL $OS $PLATFORM `sha256sum $DLFILE | awk '{print $1}'`
 }
 
 dl darwin amd64 tgz
@@ -21,5 +24,5 @@ dl linux arm tgz
 dl linux arm64 tgz
 dl windows 386 zip
 dl windows amd64 zip
-sha256sum $DIR/hub-*-$VER.*
+
 
